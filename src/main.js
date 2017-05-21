ecc643ae1a78808409d8eb8c0347289f73431435
tackeyy <mailto.takita.yusuke@gmail.com>
2017-05-21 11:32:04 +0900

    Add mdl

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import VueMdl from 'vue-mdl'
+Vue.use(VueMdl)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
