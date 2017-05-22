7cf1372c7b169719e675e08dba952879730104cb
tackeyy <mailto.takita.yusuke@gmail.com>
2017-05-22 22:11:02 +0900

    Add header

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <!-- Title -->
+        <span class="mdl-layout-title">vue-cli-app</span>
+        <!-- Add spacer, to align navigation to the right -->
+        <div class="mdl-layout-spacer"></div>
+      </div>
+    </header>
+  <div class="mdl-layout__drawer">
+    <span class="mdl-layout-title">vue-cli-app</span>
+    <nav class="mdl-navigation">
+      <a class="mdl-navigation__link" href="">Link</a>
+      <a class="mdl-navigation__link" href="">Link</a>
+      <a class="mdl-navigation__link" href="">Link</a>
+      <a class="mdl-navigation__link" href="">Link</a>
+    </nav>
+  </div>
+  </div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
