28cdac8b47e14015181fe535aea82ff0d5607c67
tackeyy <mailto.takita.yusuke@gmail.com>
2017-05-25 22:04:16 +0900

    Make header and header menue components

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,7 @@
 <template>
    <div id="app" class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-    <header class="mdl-layout__header">
-      <div class="mdl-layout__header-row">
-        <!-- Title -->
-        <span class="mdl-layout-title">vue-cli-app</span>
-        <!-- Add spacer, to align navigation to the right -->
-        <div class="mdl-layout-spacer"></div>
-      </div>
-    </header>
-    <div class="mdl-layout__drawer">
-      <span class="mdl-layout-title">vue-cli-app</span>
-      <nav class="mdl-navigation">
-        <router-link to="/lists" class="mdl-navigation__link">Go to Foo</router-link>
-        <a class="mdl-navigation__link" href="">Link</a>
-        <a class="mdl-navigation__link" href="">Link</a>
-        <a class="mdl-navigation__link" href="">Link</a>
-      </nav>
-    </div>
+    <vue-header></vue-header>
+    <vue-header-toggle></vue-header-toggle>
     <main class="mdl-layout__content">
       <div class="page-content">
         <router-view></router-view>
@@ -28,6 +13,7 @@
 
 <script>
 import VueHeader from './components/Header'
+import VueHeaderToggle from './components/HeaderToggle'
 import VueFooter from './components/Footer'
 import VueList from './components/List'
 
@@ -35,6 +21,7 @@ export default {
   name: 'app',
   components: {
     VueHeader,
+    VueHeaderToggle,
     VueFooter,
     VueList
   }
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,43 +1,12 @@
 <template>
-  <div>
-    <header class="mdl-layout__header">
-      <div class="mdl-layout__header-row">
-        <!-- Title -->
-        <span class="mdl-layout-title">vue-cli-app</span>
-        <!-- Add spacer, to align navigation to the right -->
-        <div class="mdl-layout-spacer"></div>
-      </div>
-    </header>
-    <div class="mdl-layout__drawer">
+  <header class="mdl-layout__header">
+    <div class="mdl-layout__header-row">
+      <!-- Title -->
       <span class="mdl-layout-title">vue-cli-app</span>
-      <nav class="mdl-navigation">
-        <router-link to="/lists" class="mdl-navigation__link">Go to Foo</router-link>
-        <a class="mdl-navigation__link" href="">Link</a>
-        <a class="mdl-navigation__link" href="">Link</a>
-        <a class="mdl-navigation__link" href="">Link</a>
-      </nav>
+      <!-- Add spacer, to align navigation to the right -->
+      <div class="mdl-layout-spacer"></div>
     </div>
-    <header class="mdl-layout__header">
-      <div class="mdl-layout__header-row">
-        <!-- Title -->
-        <span class="mdl-layout-title">HTML5 Tutorial</span>
-        <!-- Add spacer, to align navigation to the right -->
-        <div class="mdl-layout-spacer"></div>
-        <!-- Navigation -->
-        <nav class="mdl-navigation">
-          <a class="mdl-navigation__link" href="" style="color:gray">Home</a>
-          <a class="mdl-navigation__link" href="" style="color:gray">About</a>
-        </nav>
-      </div>
-    </header>
-    <div class="mdl-layout__drawer">
-      <span class="mdl-layout-title">HTML5 Tutorial</span>
-      <nav class="mdl-navigation">
-        <a class="mdl-navigation__link" href="">Home</a>
-        <a class="mdl-navigation__link" href="">About</a>
-      </nav>
-    </div>
-  </div>
+  </header>
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
